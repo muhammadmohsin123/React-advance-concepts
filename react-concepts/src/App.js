2decb35cca18a4d71afb3a775cae36efc047f797
muhammadmohsin123 <mohsinsurani12@gmail.com>
2021-08-04 13:18:41 +0500

    pagination

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -1,14 +1,39 @@
+import React, { useState, useEffect } from "react";
+import Pagination from "./Pagination";
+import Post from "./Posts";
+const url = "https://jsonplaceholder.typicode.com/posts";
 
-import './App.css';
-import data from './data.json'
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
 
-function App() {
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error("something went wrong while requesting posts");
+      })
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message));
+  }, []);
+
+  if (error) return <h1>{error}</h1>;
+
   return (
-    <div className="App">
-      {data.map(el=><h3>{el.Title}</h3>)}
-      
+    <div>
+      {posts.length > 0 ? (
+        <>
+          <Pagination
+            data={posts}
+            RenderComponent={Post}
+            title="Posts"
+            pageLimit={5}
+            dataLimit={10}
+          />
+        </>
+      ) : (
+        <h1>No Posts to display</h1>
+      )}
     </div>
   );
 }
-
-export default App;
